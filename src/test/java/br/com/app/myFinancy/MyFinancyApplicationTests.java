24ecf280e2de02f14fc8a59003258168540e2a30
Marcos Vinicius Santos Brito <marcos_aha2008@hotmail.com>
2022-08-31 15:59:18 -0300

    first commit

---
+++ src/test/java/br/com/app/myFinancy/MyFinancyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.app.myFinancy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFinancyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
